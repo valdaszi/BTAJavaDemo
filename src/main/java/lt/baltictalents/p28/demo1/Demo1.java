a471707ca91534fe435e117b133cd024b031ce95
Valdas <valdas@e-servisas.lt>
2020-12-07 11:45:58 +0200

    uzkomentuota JavaFX

--- src/main/java/lt/baltictalents/p28/demo1/Demo1.java
+++ src/main/java/lt/baltictalents/p28/demo1/Demo1.java
@@ -1,29 +1,29 @@
-package lt.baltictalents.p28.demo1;
-
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Demo1 extends Application {
-
-    public static void main(String... args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> System.out.println("Hello World!"));
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
-
-}
+//package lt.baltictalents.p28.demo1;
+//
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.scene.control.Button;
+//import javafx.scene.layout.StackPane;
+//import javafx.stage.Stage;
+//
+//public class Demo1 extends Application {
+//
+//    public static void main(String... args) {
+//        launch(args);
+//    }
+//
+//    @Override
+//    public void start(Stage primaryStage) throws Exception {
+//
+//        primaryStage.setTitle("Hello World!");
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(event -> System.out.println("Hello World!"));
+//
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        primaryStage.setScene(new Scene(root, 300, 250));
+//        primaryStage.show();
+//    }
+//
+//}
